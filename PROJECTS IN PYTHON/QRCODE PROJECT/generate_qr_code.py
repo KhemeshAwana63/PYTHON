3965c20170373b4785d0dd01d0c7069bc123004e
KhemeshAwana63 <khemeshawana2005@gmail.com>
2025-02-28 05:58:34 +0530

    some modification

--- PROJECTS IN PYTHON/QRCODE PROJECT/generate_qr_code.py
+++ PROJECTS IN PYTHON/QRCODE PROJECT/generate_qr_code.py
@@ -1,25 +1,26 @@
 """i am installing both qrcode and another library pillow that is used for
 image handling"""
 #importing qrcode
-import qrcode
-data  = "https://chatgpt.com/"
+def GENERATOR():
+    import qrcode
+    email = "mailto:khemeshawana92@gmail.com"
 
-#creating a QRCODE instance
-qr = qrcode.QRCode(version=1,  #gr code complexity
-error_correction=qrcode.constants.ERROR_CORRECT_L, #low error correction
-box_size=10,     #this is the size of qr code in pixels
-border=4     #border thickness and 4 is by default
-)
+    #creating a QRCODE instance
+    qr = qrcode.QRCode(version=1,  #gr code complexity
+    error_correction=qrcode.constants.ERROR_CORRECT_L, #low error correction
+    box_size=10,     #this is the size of qr code in pixels
+    border=4     #border thickness and 4 is by default
+    )
 
-#now we will add data to this instance
-qr.add_data(data)     #adds the data
-qr.make(fit=True)
+    #now we will add data to this instance
+    qr.add_data(email)     #adds the data
+    qr.make(fit=True)
 
-#now we are going to make image
-img  = qr.make_image(fill="black",back_color="white")
+    #now we are going to make image
+    img  = qr.make_image(fill="black",back_color="white")
 
-#save the QRCODE instance as an image, we have already given the data to it
-img.save("qrcode.png")
+    #save the QRCODE instance as an image, we have already given the data to it
+    img.save("email.png")
 
-#show qrcode
-img.show()
+    #show qrcode
+    img.show()
